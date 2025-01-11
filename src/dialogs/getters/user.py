edd5ad57ae597701d80c7c5883c4e383e2472dff
vladn <vl.smail@yandex.ru>
2025-01-11 20:14:07 +0300

    Commit

--- src/dialogs/getters/user.py
+++ src/dialogs/getters/user.py
@@ -1,3 +1,5 @@
+from config import Config
+
 from aiogram_dialog import DialogManager
 
 from src.database.requests.user import db_get_user
@@ -7,4 +9,9 @@ from src.database.requests.user import db_get_user
 async def user_getter(dialog_manager: DialogManager, **kwargs):
     telegram_id = dialog_manager.event.from_user.id
     user_dict = await db_get_user(telegram_id=telegram_id)
+
+    # Проверяем, является ли пользователь администратором
+    is_admin = telegram_id in Config.admin_ids
+    user_dict['is_admin'] = is_admin
+
     return user_dict
